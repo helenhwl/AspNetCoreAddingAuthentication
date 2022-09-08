f600246041d3420ce71e32c453a42d8a38cca22d
Helen Hua <wenhua@microsoft.com>
2022-09-08 21:09:48 +0800

    update module 4

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -27,16 +27,30 @@ namespace WishList.Controllers
 
         [HttpGet]
         [AllowAnonymous]
-        public IActionResult HttpGetRegister()
+        public IActionResult Register()
         {
             return View();
         }
 
         [HttpPost]
         [AllowAnonymous]
-        public IActionResult HttpPostRegister(RegisterViewModel register)
+        public IActionResult Register(RegisterViewModel model)
         {
-            return View();
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var result = _userManager.CreateAsync(new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.Password).Result;
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("Password", error.Description);
+                }
+                return View(model);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
